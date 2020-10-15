a95f18ca1d8c005a802067acb9cc44eaa919747a
Artem <artemtarasov678@gmail.com>
2020-10-15 22:34:19 +0700

    added paramterized types and implemented Iterator interface

--- Task_2_1/src/main/java/Stack.java
+++ Task_2_1/src/main/java/Stack.java
@@ -1,28 +1,36 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TransferQueue;
 
-public class Stack {
-    private int[] arr;
+public class Stack<Type> implements Iterator<Type> {
+    private Type[] arr;
     private int end;
 
     /**
      * Create stack with buffer with 10 elem size
      */
+    @SuppressWarnings("unchecked")
     Stack() {
-        arr = new int[10];
+        arr = (Type[])new Object[10];
         end = 0;
     }
 
     /**
      * @param size initial value of stack buffer
      */
+    @SuppressWarnings("unchecked")
     Stack(int size) {
-        arr = new int[size];
+        if(size < 0){
+            throw new NegativeArraySizeException("You cannot create array with negative size");
+        }
+        arr = (Type[]) new Object[size];
         end = 0;
     }
 
     private void resize() {
-        if (end == arr.length) {
+        if (end >= arr.length - 1) {
             arr = Arrays.copyOf(arr, arr.length * 2);
         }
     }
@@ -30,16 +38,16 @@ public class Stack {
     /**
      * @return top of the stack
      */
-    public int pop() {
+    public Type pop() {
         return arr[--end];
     }
 
     /**
      * @param value push value to the stack
      */
-    public void push(int value) {
-        arr[end++] = value;
+    public void push(Type value) {
         resize();
+        arr[end++] = value;
     }
 
     /**
@@ -47,5 +55,18 @@ public class Stack {
      */
     public int count() {
         return end;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return end > 0;
+    }
+
+    @Override
+    public Type next() {
+        if(!hasNext()){
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return pop();
     }
 }
