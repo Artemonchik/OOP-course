4a79e4984be342e8a00b1fd60ded696925488eb6
Artem <artemtarasov678@gmail.com>
2020-11-27 00:12:17 +0700

    Some corrections and implementation task query tests
    Calendar static method addParams was corrected
    Was added all queries from the Task_3_1

--- Task_3_1/src/main/java/Calendar.java
+++ Task_3_1/src/main/java/Calendar.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+
 import java.time.DateTimeException;
+import java.util.Date;
 import java.util.Objects;
+import java.util.stream.Stream;
+import java.util.zip.DataFormatException;
 
 /**
  * This class describes dates: YYYY-MM-DD from 0000-01-01
@@ -12,23 +17,49 @@ public class Calendar {
 
     private final int[] dayNumsInMonth = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    private int leapCount() {
+        return (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400;
+    }
+
+    private boolean isLeap() {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+    }
+
+    private void setLeapState() {
+        if (isLeap()) {
+            dayNumsInMonth[2] = 29;
+        } else {
+            dayNumsInMonth[2] = 28;
+        }
+    }
+
+
     /**
      * Set date to year-month-day
      *
      * @param year  - integer value in [0, MAX_INT/2]
-     * @param month - month in [1, MAX_INT]
-     * @param day   - day in [0, MAX_INT]
+     * @param month - month in [1, 12]
+     * @param day   - day in [0, MAX_DAY], where MAX_DAY depends on month
      * @apiNote you can call by for example this way: Calendar(2020, 5, 32), and date will be set in 2020-06-01.
      * this constructor implements autocorrection
      */
 
     public Calendar(int year, int month, int day) {
-//        year += (month - 1) / 12 + 1;
-//        month = (month - 1) % 12 + 1;
+        if (year < 0 || year > Integer.MAX_VALUE / 2) {
+            throw new DataValidationException("Year must be between [0, MAX_INT/2]");
+        }
         this.year = year;
+        if (month < 1 || month > 12) {
+            throw new DataValidationException("Month must be between [1, 12]");
+        }
         this.month = month;
+        setLeapState();
+        if (day < 1 || day > dayNumsInMonth[month]) {
+            throw new DataValidationException(
+                    String.format("Day must be between [1, %d] for this specified month", dayNumsInMonth[month]));
+        }
         this.day = day;
-
     }
 
     /**
@@ -36,6 +67,9 @@ public class Calendar {
      */
 
     public Calendar(int days) {
+        if (days < 0) {
+            throw new DataValidationException("Days must be non negative value");
+        }
         year = 1;
         year += (days - 1) / (400 * 365 + 97) * 400;
         days = (days - 1) % (400 * 365 + 97) + 1;
@@ -65,23 +99,6 @@ public class Calendar {
         return day;
     }
 
-    private int leapCount() {
-        return (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400;
-    }
-
-    private boolean isLeap() {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-
-    }
-
-    private void setLeapState() {
-        if (isLeap()) {
-            dayNumsInMonth[2] = 29;
-        } else {
-            dayNumsInMonth[2] = 28;
-        }
-    }
-
     public int toDays() {
         int days = 0;
         days += (year - 1) * 365 + leapCount();
@@ -108,32 +125,23 @@ public class Calendar {
     }
 
     /**
-     * @param calendar the Calendar instance you want to add params
-     * @param year     year you want to add
-     * @param month    month you want to add
-     * @param day      day you want to add
-     * @return new Calendar instance with increased params from calendar
+     * @param year  year you want to add
+     * @param month month you want to add
+     * @param day   day you want to add
      */
-    public static Calendar addParams(Calendar calendar, int year, int month, int day) {
-        Calendar tempDate = new Calendar(year, month, day);
-        int days = calendar.toDays() + tempDate.toDays();
-        return new Calendar(days);
-    }
-
-    /**
-     * @param calendar the Calendar instance you want to subtract params
-     * @param year     year you want to subtract
-     * @param month    month you want to subtract
-     * @param day      day you want to subtract
-     * @return new Calendar instance with reduced params from calendar
-     */
-    public static Calendar subParams(Calendar calendar, int year, int month, int day) {
-        Calendar tempDate = new Calendar(year, month, day);
-        int days = calendar.toDays() - tempDate.toDays();
-        if (days < 0) {
-            throw new DateTimeException("calendar date should be greater then date from other params");
+    public void addParams(int year, int month, int day) {
+        this.year += year;
+        this.year += (this.month + month - 1) / 12;
+        this.month = (this.month + month - 1) % 12 + 1;
+        this.day += day;
+        setLeapState();
+        while (this.day > dayNumsInMonth[this.month]){
+            this.month++;
+            this.day -= dayNumsInMonth[month];
+            this.year += (this.month + month - 1) / 12;
+            this.month = (this.month + month - 1) % 12 + 1;
+            setLeapState();
         }
-        return new Calendar(days);
     }
 
     @Override
--- Task_3_1/src/test/java/CalendarTest.java
+++ Task_3_1/src/test/java/CalendarTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+
+import java.util.zip.DataFormatException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +11,8 @@ public class CalendarTest {
         assertEquals(month, calendar.getMonth());
         assertEquals(day, calendar.getDay());
     }
-    private void calendAssert(Calendar cal1, Calendar cal2){
+
+    private void calendAssert(Calendar cal1, Calendar cal2) {
         calendAssert(cal2, cal1.getYear(), cal1.getMonth(), cal1.getDay());
     }
 
@@ -34,14 +38,6 @@ public class CalendarTest {
         Calendar cal9 = new Calendar(1975, 2, 26);
         Calendar cal10 = new Calendar(2531, 3, 13);
         assertEquals(203090, cal10.toDays() - cal9.toDays());
-
-        Calendar cal11 = new Calendar(1975, 12, 6);
-        Calendar cal12 = new Calendar(3121, 11, 30);
-        assertEquals(418562,Calendar.subDate(cal12, cal11));
-
-        Calendar cal13 = new Calendar(1, 1, 1);
-        Calendar cal14 = new Calendar(3, 1, 1);
-        assertEquals(365*2, Calendar.subDate(cal14, cal13));
     }
 
     @Test
@@ -62,5 +58,69 @@ public class CalendarTest {
         Calendar cal3 = new Calendar(2342, 1, 28);
         Calendar cal4 = new Calendar(cal1.toDays());
         calendAssert(cal1, cal2);
+    }
+
+    @Test
+    public void arithmeticOperationTest() {
+
+        Calendar cal11 = new Calendar(1975, 12, 6);
+        Calendar cal12 = new Calendar(3121, 11, 30);
+        assertEquals(418562, Calendar.subDate(cal12, cal11));
+
+        Calendar cal13 = new Calendar(1, 1, 1);
+        Calendar cal14 = new Calendar(3, 1, 1);
+        assertEquals(365 * 2, Calendar.subDate(cal14, cal13));
+
+        Calendar cal15 = new Calendar(1975, 12, 6);
+        int year15 = 3112;
+        int month15 = 11;
+        int day15 = 30;
+        cal15.addParams(year15, month15, day15);
+        calendAssert(cal15, 5089, 11, 6);
+    }
+
+    @Test
+    public void incorrectParamsTest(){
+        try {
+            Calendar cal = new Calendar(2020, 2, 30);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException err){
+            assertEquals("Day must be between [1, 29] for this specified month", err.getMessage());
+        }
+
+        try {
+            Calendar cal = new Calendar(-5, 2, 3);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException err){
+            assertEquals("Year must be between [0, MAX_INT/2]", err.getMessage());
+        }
+
+        try {
+            Calendar cal = new Calendar(Integer.MAX_VALUE/2 + 1, 2, 3);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException err){
+            assertEquals("Year must be between [0, MAX_INT/2]", err.getMessage());
+        }
+
+        try {
+            Calendar cal = new Calendar(3000, 0, 3);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException err){
+            assertEquals("Month must be between [1, 12]", err.getMessage());
+        }
+
+        try {
+            Calendar cal = new Calendar(3000, 13, 3);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException err){
+            assertEquals("Month must be between [1, 12]", err.getMessage());
+        }
+
+        try {
+            Calendar cal = new Calendar(-5);
+            fail("There must be DataValidateException");
+        }catch (DataValidationException ignored){
+
+        }
     }
 }
